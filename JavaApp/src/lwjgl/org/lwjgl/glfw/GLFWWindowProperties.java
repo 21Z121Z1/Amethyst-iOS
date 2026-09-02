64ae26c9f2d8d93809f966a770d564262558eeaa
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-09-03 02:53:01 +0800

    fix: initialize GLFW input mode defaults

--- JavaApp/src/lwjgl/org/lwjgl/glfw/GLFWWindowProperties.java
+++ JavaApp/src/lwjgl/org/lwjgl/glfw/GLFWWindowProperties.java
@@ -11,6 +11,16 @@ public class GLFWWindowProperties {
     public long monitor;
     public Map<Integer, Integer> inputModes = new HashMap<>();
     public Map<Integer, Integer> windowAttribs = new HashMap<>();
+
+    public GLFWWindowProperties() {
+        inputModes.put(GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL);
+        inputModes.put(GLFW.GLFW_STICKY_KEYS, GLFW.GLFW_FALSE);
+        inputModes.put(GLFW.GLFW_STICKY_MOUSE_BUTTONS, GLFW.GLFW_FALSE);
+        inputModes.put(GLFW.GLFW_LOCK_KEY_MODS, GLFW.GLFW_FALSE);
+        inputModes.put(GLFW.GLFW_RAW_MOUSE_MOTION, GLFW.GLFW_FALSE);
+        inputModes.put(GLFW.GLFW_UNLIMITED_MOUSE_BUTTONS, GLFW.GLFW_FALSE);
+        inputModes.put(GLFW.GLFW_IME, GLFW.GLFW_FALSE);
+    }
     
     @Override
     public String toString() {
